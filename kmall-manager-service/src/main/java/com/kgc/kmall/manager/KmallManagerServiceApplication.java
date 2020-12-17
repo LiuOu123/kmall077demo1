8dd68860feac48bdc6edebefd5e542c5b2574649
liuou <1367423614@qq.com>
2020-12-17 18:34:52 +0800

    最终三

--- kmall-manager-service/src/main/java/com/kgc/kmall/manager/KmallManagerServiceApplication.java
+++ kmall-manager-service/src/main/java/com/kgc/kmall/manager/KmallManagerServiceApplication.java
@@ -1,8 +1,9 @@
 package com.kgc.kmall.manager;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.kgc.kmall.manager.mapper")
 @SpringBootApplication
 public class KmallManagerServiceApplication {
 
